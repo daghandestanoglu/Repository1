9d0acb102a3ae0225f12afdd7eadfbea8815bb7a
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-02-22 12:36:23 +0300

    Add files via upload

--- Ana_Ekran.py
+++ Ana_Ekran.py
@@ -3,13 +3,16 @@ print("║        UYGULAMA        ║")
 print("╠════════════════════════╣")
 print("║ 1-) Hesap Makinası     ║")
 print("║ 2-) Oyunlar            ║")
+print("║ 3-) Çizimler           ║")
 print("║                        ║")
 print("║                        ║")
 print("║                        ║")
 print("║                        ║")
-print("║                        ║")
-print("║                        ║")
+print("║ C-) Çıkış              ║")
 print("╚════════════════════════╝")
+print("Seçiminiz nedir?: ",end="")
+Secim= input()
+print(f"{Secim}. seçeneği seçtiniz.")
 # 201 ╔
 # 205 ═
 # 187 ╗
