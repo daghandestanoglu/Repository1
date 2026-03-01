1447328b7c0186993b1a290cb8b5f778dcfec430
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-03-01 13:08:11 +0300

    123

--- Ana_Ekran.py
+++ Ana_Ekran.py
@@ -1,27 +1,37 @@
-print("╔════════════════════════╗")
-print("║        UYGULAMA        ║")
-print("╠════════════════════════╣")
-print("║ 1-) Hesap Makinası     ║")
-print("║ 2-) Oyunlar            ║")
-print("║ 3-) Çizimler           ║")
-print("║                        ║")
-print("║                        ║")
-print("║                        ║")
-print("║                        ║")
-print("║ C-) Çıkış              ║")
-print("╚════════════════════════╝")
-print("Seçiminiz nedir?: ", end="")
-Secim = input()
-print(f"{Secim}. seçeneği seçtiniz.")
-if Secim == "1":
-    print("Hesap Makinası seçtiniz.")
-    import Moduller.SubInterface1
-
-# 201 ╔
-# 205 ═
-# 187 ╗
-# 186 ║
-# 200 ╚
-# 188 ╝
-# 185 ╣
-# 204 ╠
+def Ana_Ekran():
+    print("╔════════════════════════╗")
+    print("║        UYGULAMA        ║")
+    print("╠════════════════════════╣")
+    print("║ 1-) Hesap Makinası     ║")
+    print("║ 2-) Oyunlar            ║")
+    print("║ 3-) Çizimler           ║")
+    print("║                        ║")
+    print("║                        ║")
+    print("║                        ║")
+    print("║                        ║")
+    print("║ 0-) Çıkış              ║")
+    print("╚════════════════════════╝")
+    print("Seçiminiz nedir?: ", end="")
+    Secim = str(input())
+    print(f"{Secim}. seçeneği seçtiniz.")
+    if Secim == "1":
+        print("Hesap Makinası seçtiniz.")
+        import Moduller.SubInterface1
+    elif Secim == "2":
+        print("Oyunlar seçtiniz.")
+        import Moduller.SubInterface2
+    #elif Secim == "3":
+        #print("Çizimler seçtiniz.")
+        #import Moduller.SubInterface3   
+    elif Secim == "0":
+        print("Çıkış yapılıyor...")
+    else:
+        print("Geçersiz seçim. Lütfen tekrar deneyin.")
+    # 201 ╔
+    # 205 ═
+    # 187 ╗
+    # 186 ║
+    # 200 ╚
+    # 188 ╝
+    # 185 ╣
+    # 204 ╠
--- Moduller/SubInterface1.py
+++ Moduller/SubInterface1.py
@@ -8,8 +8,28 @@ print("║ 4-) Çarpma             ║")
 print("║                        ║")
 print("║                        ║")
 print("║                        ║")
-print("║                        ║")
+print("║  0-Geri Dön            ║")
 print("╚════════════════════════╝")
+x=float(input("Birinci sayıyı giriniz: "))
+y=float(input("İkinci sayıyı giriniz: "))
 print("Seçiminiz nedir?: ",end="")
-Secim= input()
-print(f"{Secim}. seçeneği seçtiniz.")
+Secim1= input()
+print(f"{Secim1}. seçeneği seçtiniz.")
+if Secim1 == "1":
+    print(f"{x} + {y} = {x+y}")
+elif Secim1 == "2":
+    print(f"{x} - {y} = {x-y}")
+elif Secim1 == "3":
+    if y != 0:
+        print(f"{x} / {y} = {x/y}")
+    else:
+        print("Bir sayı sıfıra bölünemez.")
+elif Secim1 == "4":
+    print(f"{x} * {y} = {x*y}")
+elif Secim1 == "0":
+    print("Ana menüye dönülüyor...")
+    import Ana_Ekran
+else:
+    print("Geçersiz seçim. Lütfen tekrar deneyin.")
+    ##import Moduller.SubInterface1
+    
