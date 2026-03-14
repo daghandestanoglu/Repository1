7c1ad0fd158038c16e0e5681f9a807148e459e09
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-03-14 13:00:50 +0300

    wwwww

--- sinif.py
+++ sinif.py
@@ -1,6 +1,13 @@
-d=open("Deneme1.txt","w")
-d.write(f"Ad {input('isim?:')}")
+d=open("Deneme1.txt","w",encoding="utf-8")
+isim=input("İsim:").strip().lower()
+d.write(f"Ad:\t{isim}")
 d.write("\n")
-d.write(f"Soyad {input('Soyad?:')}")
+
+Soyad=input("Soyad:").strip().lower()
+d.write(f"Soyad:\t{Soyad}")
 d.write("\n")
-d.write(f"Numara {input('Numara?:')}")
+
+Numara=input("Numara:").strip().lower()
+d.write(f"Numara:\t{Numara}")
+d.write("\n")
+d.close()
