3c1a17750b55375084cece6b79e693cf525cd2c7
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-03-29 13:08:10 +0300

    kkkk

--- deneme1/aaa.py
+++ deneme1/aaa.py
@@ -9,13 +9,22 @@ d.close()
 d = open("deneme4.txt","r")
 print(d.read())
 d.close()
-time.sleep(5)  # Always close the file first
+time.sleep(3)  # Always close the file first
 d = open("deneme4.txt","a")
 d.write("Ekleme?\n")
 d.close()
 d = open("deneme4.txt","r")
 print(d.read())
 d.close()
-time.sleep(5)  # Wait 5 seconds
+with open("deneme4.txt","a") as d:
+    d.write(input("Bi şeyler yaz:").strip().lower())
+with open("deneme4.txt","r") as d:
+    print(d.read())
+    d.seek(0)
+    for i, satir in enumerate(d, start=1):  # enumerate satır numarası verir
+        if "ali" in satir.lower():
+            print(f"Bulundu: '{satir.strip()}'  -> Satır numarası: {i}")  
+time.sleep(3)  # Wait 5 seconds
+
 
 os.remove("deneme4.txt")  # Delete the file
