8e2d3c39436e4fb3459dff704d0457ea182151a7
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-03-29 11:47:58 +0300

    da

--- deneme1/aaa.py
+++ deneme1/aaa.py
@@ -5,8 +5,17 @@ d = open("deneme4.txt","w")
 d.write("Dahan\n")
 d.write("Giray\n")
 d.write("Destan\n")
-d.close()  # Always close the file first
-
+d.close()
+d = open("deneme4.txt","r")
+print(d.read())
+d.close()
+time.sleep(5)  # Always close the file first
+d = open("deneme4.txt","a")
+d.write("Ekleme?\n")
+d.close()
+d = open("deneme4.txt","r")
+print(d.read())
+d.close()
 time.sleep(5)  # Wait 5 seconds
 
 os.remove("deneme4.txt")  # Delete the file
