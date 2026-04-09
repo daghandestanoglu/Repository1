3c3ff4d4715c2197bf3fa38f7e80aabdcb5887b5
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-04-09 20:15:31 +0300

    dd

--- Proje2/Modl4.py
+++ Proje2/Modl4.py
@@ -2,7 +2,7 @@ def Urunoku():
     DOSYA_ADI = "envanter.txt"
     urunler=[]
     try:
-        with open(DOSYA_ADI,r, encoding="utf-8") as f:
+        with open(DOSYA_ADI,"r", encoding="utf-8") as f:
             for satir in f:
                 satir=satir.strip()
                 if satir:
