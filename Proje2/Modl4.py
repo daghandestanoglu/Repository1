7ccc5a35c2f5f2e5478083e734cb2f1c9594524c
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-04-09 20:14:54 +0300

    d

--- Proje2/Modl4.py
+++ Proje2/Modl4.py
@@ -1,2 +1,13 @@
 def Urunoku():
-    
+    DOSYA_ADI = "envanter.txt"
+    urunler=[]
+    try:
+        with open(DOSYA_ADI,r, encoding="utf-8") as f:
+            for satir in f:
+                satir=satir.strip()
+                if satir:
+                    aD, sayi = satir.split(",",1)
+                    urunler.append({"ad":aD,"sayi":sayi})
+    except FileNotFoundError:
+        pass
+    return urunler
