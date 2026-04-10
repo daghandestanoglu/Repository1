3b9ea66e6c19228f280274411d26183e45c47570
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-04-10 15:53:58 +0300

    tre

--- Proje2/Modl3.py
+++ Proje2/Modl3.py
@@ -1,2 +1,6 @@
-def UrunYaz():
-    
+def UrunYaz(urunler):
+    DOSYA_ADI = "envanter.txt"
+    with open("envanter.txt","w",encoding="utf-8") as d:
+        for urun in urunler:
+            d.write(f"{urun['ad']},{urun['sayi']}\n")
+            
