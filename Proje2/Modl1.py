db54650ddb0eb277848993c666eca2e01f19ea70
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-04-10 16:29:52 +0300

    m

--- Proje2/Modl1.py
+++ Proje2/Modl1.py
@@ -2,3 +2,20 @@ import Proje2.Modl4
 def UrunAra():
     aranan = input("Aranacak ürünün ismi(Boşluk bırakırsanız hepsini listeler):").strip().lower()
     Urunler= Proje2.Modl4.Urunoku()
+    if not Urunler:
+        print("Envanter boş.")
+        return
+    sonuclar = []
+    for k in Urunler:
+        if aranan.lower() in k["ad"].lower():
+            sonuclar.append(k)
+    if not sonuclar:
+        print(f"{aranan} ile eşleşen bulunamadı.")
+    else:
+        print(f"\n{'-'*35}")
+        print(f"{'Ad':20}{'Sayı'}")
+        print(f"{'-'*35}")
+        for k in sonuclar:
+            print(f"{k['ad']:<20}{k['sayi']}")
+        print(f"{'-'*35}")
+        print(f"Toplam {len(sonuclar)} kişi.")
