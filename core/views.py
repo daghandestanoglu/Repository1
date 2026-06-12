41be3f4280340d22eb2d71638ee4851eb7e0ca14
daghandestanoglu <daghandestanoglu1@gmail.com>
2026-06-12 19:08:43 +0300

    jhgfd

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Urun
 
 def dashboard(request):
@@ -10,3 +10,35 @@ def dashboard(request):
         'urunler': urunler,  # Tüm ürün listesini şablona fırlatıyoruz
     }
     return render(request, 'dashboard.html', context)
+# DOĞRU SATIR
+def urun_ekle(request):
+    if request.method == 'POST':
+        urun_adi = request.POST.get('urun_adi').strip().lower()
+        urun_sayisi = request.POST.get('urun_sayisi')
+        
+        if urun_adi and urun_sayisi:
+            Urun.objects.create(ad=urun_adi, sayi=int(urun_sayisi))
+            return redirect('dashboard')
+            
+    return render(request, 'urun_ekle.html')
+            
+    return render(request, 'urun_ekle.html')
+def urun_sil(request, urun_id):
+    if request.method == 'POST':
+        # URL'den gelen id'ye göre ürünü bul ve MySQL'den sil (DELETE)
+        urun = Urun.objects.get(id=urun_id)
+        urun.delete()
+    return redirect('dashboard')
+def urun_guncelle(request, urun_id):
+    # İlgili ürünü veritabanından getir
+    urun = Urun.objects.get(id=urun_id)
+    
+    if request.method == 'POST':
+        yeni_sayi = request.POST.get('urun_sayisi')
+        if yeni_sayi:
+            # Kolonu güncelle ve MySQL'e kaydet (UPDATE)
+            urun.sayi = int(yeni_sayi)
+            urun.save()
+            return redirect('dashboard')
+            
+    return render(request, 'urun_guncelle.html', {'urun': urun})
--- envanter_web/urls.py
+++ envanter_web/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import dashboard
+from core.views import dashboard, urun_ekle, urun_sil, urun_guncelle # Yeni fonksiyonu çağır
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Hata buradaydı; get_urls() değil, sadece .urls olacak
+    path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
+    path('ekle/', urun_ekle, name='urun_ekle'),
+    path('sil/<int:urun_id>/', urun_sil, name='urun_sil'),
+     path('guncelle/<int:urun_id>/', urun_guncelle, name='urun_guncelle'), # Dinamik silme adresi
 ]
